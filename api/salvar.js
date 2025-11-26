8225e0b93b93195551fed63cd26f9125ad61de8e
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2025-11-26 16:05:39 -0300

    Update salvar.js

--- api/salvar.js
+++ api/salvar.js
@@ -1,6 +1,15 @@
-import clientPromise from "../../server/lib/mongodb.js";
+import clientPromise from "../lib/mongodb.js";
 
 export default async function handler(req, res) {
+  // CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
@@ -9,13 +18,11 @@ export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db("apontamentosDiarios");
 
-    const dados = req.body;
-
-    await db.collection("apontamentos").insertOne(dados);
+    await db.collection("apontamentos").insertOne(req.body);
 
     res.status(200).json({ message: "Salvo com sucesso!" });
   } catch (err) {
-    console.error(err);
+    console.error("ERRO API:", err);
     res.status(500).json({ error: "Erro ao salvar" });
   }
 }
