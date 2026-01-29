837ca091ab3fe9b2615646f1b36765bad2fa2a7b
Emerson Santos <89021451+Emersonvol@users.noreply.github.com>
2026-01-29 17:24:05 -0300

    Update index.jsx

--- src/telas/paginaPadrao/index.jsx
+++ src/telas/paginaPadrao/index.jsx
@@ -1,17 +1,6 @@
 import BarsDataset from "../../componentes/graficosMui";
 import Header from "../../componentes/Header";
-import { BarChart } from '@mui/x-charts/BarChart';
-import Box from '@mui/material/Box';
-
 import "./styled.css"
-const uData = [40, 30, 20, 27, 18, 23, 34];
-const xLabels = [
-  'Carol',
-  'Grase',
-  'Andresa',
-  'Vanda',
- 
-];
 
 function PaginaPadrao() {
   return (
@@ -26,28 +15,13 @@ function PaginaPadrao() {
           <img src="./logo-sem-fundo.png" />
           <h3>SKF</h3>
         </div>
-        <Box sx={{ width: '50%', height: 700 }}>
-          <BarChart
-            series={[
-              {
-                data: uData,
-                label: 'Saida diaria',
-                id: 'uvId',
-                yAxisId: 'rightAxisId',
-              },
-            ]}
-            xAxis={[{ data: xLabels }]}
-            yAxis={[
-              { id: 'leftAxisId', width: 50 },
-              { id: 'rightAxisId', position: 'right' },
-            ]}
-          />
-        </Box>
+       
       </section>
     </>
   )
 
 
 }
+
 
 export default PaginaPadrao;
